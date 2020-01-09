47b6e44c7aba2c0108db2638ba1cce392433fa55
Christhopher Lion <christhopherleao@icloud.com>
2020-01-09 18:05:46 +0000

    codesplit and refactor to less opinionated config

---
+++ handlers.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import _ from 'lodash';
+import yargs from 'yargs';
+
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+const args = yargs.argv._;
+
+const {
+  SLACK_AVATAR,
+  GITHUB_EVENT_PATH,
+  GITHUB_ACTOR,
+  GITHUB_EVENT_NAME,
+  GITHUB_REPOSITORY
+} = process.env;
+
+const EVENT_PAYLOAD = JSON.parse(
+  fs.readFileSync(GITHUB_EVENT_PATH, "utf8")
+);
+
+// Override Slack message
+export const getMessage = () => {
+  const DEFAULT_MESSAGE = `@${GITHUB_ACTOR} (${GITHUB_EVENT_NAME}) at ${GITHUB_REPOSITORY}`;
+
+  // If any arguments provided, parse moustaches on template string:
+  if (args.length) return _.template(args.join(" "))({ ...process.env, EVENT_PAYLOAD });
+
+  return DEFAULT_MESSAGE;
+}
+
+
+// overrides default avatar
+export const selectAvatar = () => {
+  switch (SLACK_AVATAR) {
+    case 'sender': return EVENT_PAYLOAD.sender.avatar_url
+    case 'repository': return EVENT_PAYLOAD.owner.avatar_url
+    default: return SLACK_AVATAR;
+  }
+}
---
+++ message.js
@@ -0,0 +1,44 @@
+/*
+  @description
+  Message payload to be sent with Slack webhook
+*/
+import { selectAvatar, getMessage } from "./handlers.js";
+
+const {
+  SLACK_AVATAR,
+  SLACK_USERNAME,
+  SLACK_CHANNEL,
+} = process.env;
+
+const messageSingleton = (() => {
+  let instance;
+
+  function createInstance() {
+    const message = {};
+    message.text = getMessage(); // Args || DEFAULT_MESSAGE
+
+    // override username
+    if (SLACK_USERNAME) message.username = SLACK_USERNAME;
+
+    // override channel
+    if (SLACK_CHANNEL) message.channel = SLACK_CHANNEL;
+
+    /*
+      If provided avatar add it,
+      also can use use sender, or repository image.
+      defaults to webhook slack app;
+    */
+    if (SLACK_AVATAR) message.icon_url = selectAvatar();
+
+    return message;
+  }
+
+  return {
+    get() {
+      if (!instance) instance = createInstance();
+      return instance;
+    }
+  };
+})();
+
+export default messageSingleton;
